2e9497ad1733fe1f40c3f52decbb2c495debf9cc
Yakhlef1981 <samir.yakhlef30@gmail.com>
2022-09-24 23:57:10 +0200

    mise en place basic du projet

---
+++ src/components/sliders/Sliders.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './sliders.scss';
+const Sliders = () => {
+    return (
+        <div>
+        <h1>test sliders components</h1>
+        </div>
+    )
+}
+
+export default Sliders
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sliders from '../components/sliders/Sliders';
+
+const Home = () => {
+    return (
+        <div>
+        <Sliders />
+        </div>
+    );
+};
+
+export default Home;
