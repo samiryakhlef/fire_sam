87e751a60e2a9bb2fabeff46f75932c74433e655
Yakhlef1981 <samir.yakhlef30@gmail.com>
2022-09-25 17:47:52 +0200

    mise en place du sliders

--- src/components/sliders/Sliders.jsx
+++ src/components/sliders/Sliders.jsx
@@ -1,11 +1,56 @@
 import React from 'react'
 import './sliders.scss';
+import { Carousel } from 'react-bootstrap';
+
+
 const Sliders = () => {
     return (
-        <div>
-        <h1>test sliders components</h1>
+        <div className="sliders">
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="images"
+                        src="https://picsum.photos/200"
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <div className='description'>
+                            <h3>Titre</h3>
+                            <p>description</p>
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="images"
+                        src="https://picsum.photos/200"
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <div className='description'>
+                            <h3>Titre</h3>
+                            <p>description</p>
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="images"
+                        src="https://picsum.photos/200"
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <div className='description'>
+                            <h3>Titre</h3>
+                            <p>description</p>
+                        </div>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
         </div>
-    )
+    );
 }
 
-export default Sliders
+export default Sliders;
